ed30530f70fae6759c55cdd415b4ad7334c31301
mastro <25ayushi1995@gmail.com>
2021-02-09 17:37:27 +0530

    change in controllers

--- routes/vehical.js
+++ routes/vehical.js
@@ -1,12 +1,20 @@
-const express = require("express");
-const multer = require("multer");
-const VehicalController = require("../controllers/vehical");
-const router = express.Router();
+const express = require('express')
+const multer = require('multer')
+const VehicalController = require('../controllers/vehical')
+const router = express.Router()
 
-router.get("/", VehicalController.getVehicals);
-router.post("/newvehical", VehicalController.createVehical);
-router.patch("/updatevehical/:id", VehicalController.updateVehical);
+router.get('/', VehicalController.getVehicals)
+router.post(
+  '/newvehical',
+  multer().any('vehical_image'),
+  VehicalController.createVehical
+)
+router.patch(
+  '/updatevehical/:id',
+  multer().any('vehical_image'),
+  VehicalController.updateVehical
+)
 
-router.delete("/deleteVehical/:id", VehicalController.deleteVehical);
+router.delete('/deleteVehical/:id', VehicalController.deleteVehical)
 
-module.exports = router;
+module.exports = router
--- settings/connectDB.js
+++ settings/connectDB.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
-const { DB_USERNAME, DB_PASS, DB_NAME } = process.env;
+const mongoose = require('mongoose')
+const { DB_USERNAME, DB_PASS, DB_NAME, DB_URL } = process.env
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`, {
+    await mongoose.connect(`${DB_URL}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
-    });
+    })
   } catch (error) {
-    return error;
+    return error
   }
-};
+}
 
-module.exports = connectDB;
+module.exports = connectDB
