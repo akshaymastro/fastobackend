a9ee082c2ed59fcf982f2da36a3842948e176935
mastro <25ayushi1995@gmail.com>
2021-02-09 17:36:18 +0530

    change in controllers

--- controllers/vehical.js
+++ controllers/vehical.js
@@ -1,44 +1,47 @@
-const VehicalModel = require("../model/vehical.model");
-const responseHandler = require("../helpers/responseHandler");
+const VehicalModel = require('../model/vehical.model')
+const responseHandler = require('../helpers/responseHandler')
 
 exports.createVehical = async (req, res, next) => {
   try {
-    const newVehical = await VehicalModel(req.body).save();
-    console.log(newVehical);
-    responseHandler.success(res, "Vehical Created SuccesFully", 200);
+    console.log(req.body, 'bodyyd')
+    const newVehical = await VehicalModel(req.body).save()
+    console.log(newVehical)
+    responseHandler.success(res, 'Vehical Created SuccesFully', 200)
   } catch (e) {
-    next(e);
+    next(e)
   }
-};
+}
 
 exports.updateVehical = async (req, res, next) => {
   try {
     const updateVehical = await VehicalModel.updateOne(
       { _id: req.params.id },
       { ...req.body }
-    );
-    console.log(updateVehical, "vehicall");
-    responseHandler.success(res, "Vehical Updated SuccesFully", 200);
+    )
+    console.log(updateVehical, 'vehicall')
+    responseHandler.success(res, 'Vehical Updated SuccesFully', 200)
   } catch (e) {
-    next(e);
+    next(e)
   }
-};
+}
 
 exports.deleteVehical = async (req, res, next) => {
+  console.log(req.params, 'parammsmsm')
   try {
     await VehicalModel.deleteOne({
       _id: req.params.id,
-    });
+    })
+    responseHandler.success(res, 'Vehicle Delete SuccessFully', 200)
   } catch (e) {
-    next(e);
+    next(e)
   }
-};
+}
 
 exports.getVehicals = async (req, res, next) => {
   try {
-    const vehical = await VehicalModel.find();
-    responseHandler.data(res, vehical, 200);
+    const vehical = await VehicalModel.find()
+    responseHandler.data(res, vehical, 200)
   } catch (e) {
-    next(e);
+    next(e)
   }
-};
+}
