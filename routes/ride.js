3a502670c6049d242bea859af4b2d6c3e75272cc
Akshay Sharma <akshaysharma@Akshays-MacBook-Pro.local>
2021-06-26 20:09:15 +0530

    change in ride route

--- routes/ride.js
+++ routes/ride.js
@@ -3,7 +3,7 @@ const rideController = require("../controllers/ride");
 const router = express.Router();
 
 router.post("/", rideController.GetRideList);
-
+router.post("/currentRide", rideController.getRideData);
 router.post("/userrides", rideController.GetUserRideList);
 
 router.post("/userride", rideController.GetUserRide);
@@ -13,5 +13,5 @@ router.post("/createride", rideController.CreateRide);
 
 //Route to delete ride
 router.post("/deleteride", rideController.DeleteRide);
-router.patch("/updateDriver",rideController.UpdateDriver);
+router.patch("/updateDriver", rideController.UpdateDriver);
 module.exports = router;
